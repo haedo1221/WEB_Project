4523faae626d404a68fc17f6575bdeb9c08fe201
haedo1221 <haedo1221@gmail.com>
2022-05-26 09:11:37 +0900

    2. [front] css

---
+++ src/test/java/site/metacoding/web_project/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.web_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
